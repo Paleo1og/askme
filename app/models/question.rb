d8a14caca93637e87da5512533282e4bad0e18cc
Paleo1og <itanasovich@gmail.com>
2019-04-02 17:23:49 +0700

    validation email,username

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,8 @@
 class Question < ApplicationRecord
 
+  belongs_to :user
+
+  validates :text, :user, presence: true
+  validates :text, length: { maximum: 255 } #проверка макс. длины текста
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,40 @@
+require 'openssl'
+
 class User < ApplicationRecord
 
+  ITERATIONS = 20000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
+  has_many :questions
+  attr_accessor :password
+
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
+  validates :password, presence: true, on: :create
+  validates_confirmation_of :password
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } #проверка формата емайл
+  validates :username, length: { maximum: 40 }, format: { with: /\A[a-zA-Z0-9_]+\Z/ } #проверка формата и длины юзернэйма
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    if self.password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      self.password_hash = User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST))
+    end
+  end
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email)
+
+    if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
+      user
+    else
+      nil
+    end
+  end
 end
