5da46a12baae6772b5ae99b3f0f77fcccef53cfb
Paleo1og <itanasovich@gmail.com>
2019-04-13 10:48:49 +0700

    users + sessions controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,58 @@
 class UsersController < ApplicationController
+
+  before_action :load_user, except: [:index, :create, :new]
+
+  before_action :authorize_user, except: [:index, :new, :create, :show]
+
   def index
-    @users = [
-        User.new(
-                id: 1,
-                name: 'Ivan',
-                username: 'paleo1og',
-                avatar_url: 'http://www.animated-gifs.eu/category_cartoons/avatars-100x100-cartoons-spongebob/0038.gif'
-        ),
-        User.new(
-                id: 2,
-                name: 'Misha',
-                username: 'bumbum'
-        )
-    ]
+    @users = User.all
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new
+  end
+
+  def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Данные обновлены'
+    else
+      render 'edit'
+    end
+  end
+
   def show
-    @user = User.new(
-      name: 'Ivan',
-      username: 'paleo1og',
-      avatar_url: 'http://www.animated-gifs.eu/category_cartoons/avatars-100x100-cartoons-spongebob/0038.gif'
-      )
+    @questions = @user.questions.order(created_at: :desc)
 
-    @questions = [
-        Question.new(text: 'Как дела?', created_at: Date.parse('08.04.2019')),
-        Question.new(text: 'Че кого?', created_at: Date.parse('09.04.2019')),
-      ]
+    @new_question = @user.questions.build
+  end
 
-    @new_question = Question.new
+  private
 
+  def authorize_user
+    reject_user unless @user == current_user
+  end
+
+  def load_user
+    @user ||= User.find params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :username, :avatar_url)
   end
 end
