b55f82136c0c83f409be9544c91aa32f090d423c
Paleo1og <itanasovich@gmail.com>
2019-04-14 11:38:50 +0700

    sklonenie voprosov

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,16 @@ module ApplicationHelper
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
+
+    def inclination(number, krokodil, krokodila, krokodilov)
+    number = 0 if number.nil? || !number.is_a?(Numeric)
+
+    return krokodilov if number % 100 >= 11 && number % 100 <= 14
+
+    remainder = number % 10
+
+    return krokodil if remainder == 1
+    return krokodila if remainder >= 2 && remainder <= 4
+    return krokodilov if (remainder >= 5 && remainder <= 9) || remainder.zero?
+  end
 end
