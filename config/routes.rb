309e80abe5896d570f3367078125f645d411cd2a
Paleo1og <itanasovich@gmail.com>
2019-04-21 15:43:39 +0700

    shortcut routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-
   root 'users#index'
 
+  # Ресурс пользователей (экшен destroy не поддерживается)
   resources :users, except: [:destroy]
-  resources :sessions, only: [:new, :create, :destroy]
+
+  # ресурс сессий (только три экшена :new, :create, :destroy)
+  resource :session, only: [:new, :create, :destroy]
+
+  # Ресурс вопросов (кроме экшенов :show, :new, :index)
   resources :questions, except: [:show, :new, :index]
-
-
-  get 'sing_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
-
 end
